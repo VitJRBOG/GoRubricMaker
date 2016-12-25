c3302c5cc321a310b86577da6a0a22b4d739ee2d
vitjrbog <vitjrbog@gmail.com>
2016-12-25 20:31:42 +0500

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+    public static void main(String[] args) {
+        MainClass objMainClass =
+                new MainClass();
+        objMainClass.Starter();
+    }
+
+    private void Starter() {
+
+    }
+}
