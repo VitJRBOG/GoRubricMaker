4a9a4bcf437976b6230fc186f9300aee2109478b
vitjrbog <vitjrbog@gmail.com>
2017-03-06 14:16:35 +0500

    Initial commit

---
+++ rubric_maker.py
@@ -0,0 +1,72 @@
+# coding: utf8
+
+
+def starter():
+    try:
+        open("json/questions.json", 'w')
+        open("json/loss.json", 'w')
+    except Exception as var_except:
+        print("COMPUTER: Error, " + str(var_except) + ". Exit from program...")
+        exit()
+    main_menu()
+
+
+def main_menu():
+    print("COMPUTER: You are in Main menu...")
+    print("COMPUTER [Main menu -> ]: 1 == Add post.")
+    print("COMPUTER [Main menu -> ]: 2 == Lists menu.")
+    print("COMPUTER [Main menu -> ]: 3 == File manager.")
+    print("COMPUTER [Main menu -> ]: 0 == Close program.")
+    try:
+        user_answer = raw_input("USER: [Main menu -> ]")
+        if user_answer == "0":
+            print("COMPUTER [Main menu]: Exit from program...")
+            exit()
+        else:
+            if user_answer == "1":
+                add_post()
+            else:
+                if user_answer == "2":
+                    lists_menu()
+                else:
+                    if user_answer == "3":
+                        file_manager()
+                    else:
+                        print("COMPUTER [Main menu] Unknown command. Retry query...")
+                        main_menu()
+    except Exception as var_except:
+        print("COMPUTER [Main menu]: Error, " + str(var_except) + ". Return to Main menu...")
+        main_menu()
+
+
+def add_post():
+    print("COMPUTER [Main menu -> Add post]: Here is empty. Return to Main menu...")
+    main_menu()
+
+
+def lists_menu():
+    print("COMPUTER [Main menu -> Lists menu]: Here is empty. Return to Main menu...")
+    main_menu()
+
+
+def file_manager():
+    print("COMPUTER [Main menu -> File manager]: Here is empty. Return to Main menu...")
+    main_menu()
+
+
+def add_post_set_body():
+    print("COMPUTER [Main menu -> Add post -> Body]: Here is empty. Return to Main menu...")
+    main_menu()
+
+
+def add_post_set_photo():
+    print("COMPUTER [Main menu -> Add post -> Photo]: Here is empty. Return to Main menu...")
+    main_menu()
+
+
+def add_post_set_author():
+    print("COMPUTER [Main menu -> Add post -> Author]: Here is empty. Return to Main menu...")
+    main_menu()
+
+
+starter()
