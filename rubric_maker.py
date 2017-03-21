1d21b1efe447d840634b246cdcef64ef5e3d30f1
vitjrbog <vitjrbog@gmail.com>
2017-03-21 11:46:42 +0500

    Update - created and described class "Post" in file "rubric_maker.py"; - described function "add_post()" in file "rubric_maker.py"; - created function "add_post_set_number_post(post_type, obj_post)" in file "rubric_maker.py"; - and few minor changes.

--- rubric_maker.py
+++ rubric_maker.py
@@ -24,7 +24,7 @@ def main_menu():
     print(
         "COMPUTER [Main menu -> ]: 0 == Close program.")
     try:
-        user_answer = raw_input("USER: [Main menu -> ]")
+        user_answer = raw_input("USER: [Main menu -> ] ")
         if user_answer == "0":
             print("COMPUTER [Main menu]: Exit from program...")
             exit()
@@ -49,10 +49,106 @@ def main_menu():
         main_menu()
 
 
+class Post:
+    var_number_category = 0
+    var_number_post = 0
+    var_body = ""
+    var_photo = ""
+    var_author = ""
+
+    def set_var_number_category(self, var_number_category_inner):
+        self.var_number_category = var_number_category_inner
+
+    def set_var_number_post(self, var_number_post_inner):
+        self.var_number_post = var_number_post_inner
+
+    def set_var_body(self, var_body_inner):
+        self.var_body = var_body_inner
+
+    def set_var_photo(self, var_photo_inner):
+        self.var_photo = var_photo_inner
+
+    def set_var_author(self, var_author_inner):
+        self.var_author = var_author_inner
+
+    def get_var_number_category(self):
+        return self.var_number_category
+
+    def get_var_number_post(self):
+        return self.var_number_post
+
+    def get_var_body(self):
+        var_body_this = str(self.var_number_category) + "." +\
+                        str(self.var_number_post) + ") " +\
+                        self.var_body
+        return var_body_this
+
+    def get_var_photo(self):
+        return self.var_photo
+
+    def get_var_author(self):
+        return self.var_author
+
+    def __init__(self):
+        self.var_number_category = 0
+        self.var_number_post = 0
+        self.var_body = ""
+        self.var_photo = ""
+        self.var_author = ""
+
+
 def add_post():
     print(
-        "COMPUTER [Main menu -> Add post]: Here is empty. " +
-        "Return to Main menu...")
+        "COMPUTER: You are in menu Add post...")
+    print(
+        "COMPUTER [Main menu -> ]: 1 == Add question.")
+    print(
+        "COMPUTER [Main menu -> ]: 2 == Add loss.")
+    print(
+        "COMPUTER [Main menu -> ]: 0 == Step back.")
+
+    post_type = ""
+
+    try:
+        user_answer = raw_input("USER [.. -> Add post ->]: ")
+
+        if user_answer == "1":
+            post_type = "questions"
+        else:
+            if user_answer == "2":
+                post_type = "loss"
+            else:
+                if user_answer == "0":
+                    main_menu()
+                else:
+                    print(
+                        "COMPUTER [Main menu] Unknown command. " +
+                        "Retry query...")
+                    add_post()
+    except Exception as var_except:
+        print(
+            "COMPUTER [Main menu]: Error, " + str(var_except) +
+            ". Return to Main menu...")
+        main_menu()
+
+    obj_post = Post()
+
+    if post_type == "questions":
+        obj_post.set_var_number_category(1)
+    else:
+        if post_type == "loss":
+            obj_post.set_var_number_category(2)
+        else:
+            print(
+                "COMPUTER [Main menu -> Add post -> " +
+                post_type + "]: Error, unknown category of post" +
+                ". Return to Main menu...")
+            main_menu()
+
+    obj_post.set_var_body(add_post_set_body(post_type, obj_post))
+    obj_post.set_var_photo(add_post_set_photo(post_type, obj_post))
+    obj_post.set_var_author(add_post_set_author(post_type, obj_post))
+
     main_menu()
 
 
@@ -77,7 +173,16 @@ def add_post_make_post():
     main_menu()
 
 
-def add_post_set_body(post_type):
+def add_post_set_number_post(post_type, obj_post):
+
+    print(
+        "COMPUTER [Main menu -> Lists menu]: Here is empty. " +
+        "Return to Main menu...")
+
+    main_menu()
+
+
+def add_post_set_body(post_type, obj_post):
 
     print(
         "COMPUTER [Main menu -> Add post -> " + post_type +
@@ -88,7 +193,7 @@ def add_post_set_body(post_type):
     main_menu()
 
 
-def add_post_set_photo(post_type):
+def add_post_set_photo(post_type, obj_post):
 
     print(
         "COMPUTER [Main menu -> Add post -> " + post_type +
@@ -99,7 +204,7 @@ def add_post_set_photo(post_type):
     main_menu()
 
 
-def add_post_set_author(post_type):
+def add_post_set_author(post_type, obj_post):
 
     print(
         "COMPUTER [Main menu -> Add post -> " + post_type +
